c2c6e205aa555d7788ca24b07849f51180e9f95e
bullfrog <>
2025-01-14 14:16:38 +0800

    no message

---
+++ BeeCloudKit/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BeeCloudKit
+//
+//  Created by bullfrog on 2025/1/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BeeCloudKit/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BeeCloudKit
+//
+//  Created by bullfrog on 2025/1/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
